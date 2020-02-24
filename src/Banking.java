010a3513a935d46966fcebcce3570dd0f1224af7
kopil2019 <bajgainphajindra@gmail.com>
2020-02-24 00:18:15 -0600

    This is my first project in Github. Good luck for me.

---
+++ src/Banking.java
@@ -0,0 +1,14 @@
+
+class Test {
+    static int a = 20;
+   static int b = 15;
+    void m1() {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(a+b);
+
+    }
+
+}
